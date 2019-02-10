7ea7656c5fe1643f76dbca2dbe855de53abb444c
danko <danko1@gmail.com>
2019-02-09 21:30:16 -0500

    adjusted the distance to turn wheels when each button pushed.

--- src/main/java/frc/robot/Macadamia.java
+++ src/main/java/frc/robot/Macadamia.java
@@ -55,38 +55,26 @@ public class Macadamia extends TimedRobot {
   private final int R_ENCODER_B = 3;
 
   private double leftSpeed, rightSpeed; // speed to run motors
-  private int stopLeft, stopRight;      // When robot reached its destination
+  private int stopLeft, stopRight; // When robot reached its destination
 
-  private final int X_BUTTON = 0;
-  private final int Y_BUTTON = 1;
-  private final int B_BUTTON = 2;
-  private final int A_BUTTON = 3;
-  private final int NOBUTTON = -1;
-  private int buttonPressed = NOBUTTON;
+  public class DriveAround extends Thread {
 
-
-/*public class MyThread extends Thread {
-
-  public void run(){
-    System.out.println("MyThread");
+    public void run() { // drive in square, all sides 12 inches, then go backward 12 inches
+      driveForwardInches(12);
+      turnRight();
+      driveForwardInches(12);
+      turnRight();
+      driveForwardInches(12);
+      turnRight();
+      driveForwardInches(12);
+      turnLeft();
+      driveBackwardInches(12);
+    }
   }
-    public void driveS(){
-      driveEncoder(2000,2000, 0.47,0.5);
-  }
-    public void driveRight(){
-    driveEncoder(666,-666, 0.5,-0.47);
-  }
-    public void driveBack(){
-    driveEncoder(-2000,-2000, -0.47,-0.5);
-  }
-    public void driveLeft(){
-    driveEncoder(-666,666, -0.47,0.5);
-  }
-} */
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -94,39 +82,39 @@ public class Macadamia extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    //Configure Drive
+    // Configure Drive
     leftTalon = new Talon(LEFT_TALON);
     rightTalon = new Talon(RIGHT_TALON);
     drive = new DifferentialDrive(leftTalon, rightTalon);
 
-    //Configure Ultrasonic sensors on front and rear
-      frontUltrasonic = new Ultrasonic2537(FRONT_PING, FRONT_ECHO); // ping, echo
-      rearUltrasonic = new Ultrasonic2537(REAR_PING, REAR_ECHO); // ping, echo
-      rearUltrasonic.setAutomaticMode(true);
-      
+    // Configure Ultrasonic sensors on front and rear
+    frontUltrasonic = new Ultrasonic2537(FRONT_PING, FRONT_ECHO); // ping, echo
+    rearUltrasonic = new Ultrasonic2537(REAR_PING, REAR_ECHO); // ping, echo
+    rearUltrasonic.setAutomaticMode(true);
 
-    //Configure Joystick input
+    // Configure Joystick input
     xbox = new XboxController(0);
 
-    //Configure Camera
+    // Configure Camera
     CameraServer.getInstance().startAutomaticCapture();
 
-    //Instantiate timer
+    // Instantiate timer
     timer = new Timer();
 
-    leftEnc = new Encoder (L_ENCODER_A, L_ENCODER_B);
-    rightEnc = new Encoder (R_ENCODER_A, R_ENCODER_B, true);
+    leftEnc = new Encoder(L_ENCODER_A, L_ENCODER_B);
+    rightEnc = new Encoder(R_ENCODER_A, R_ENCODER_B, true);
     leftEnc.reset();
     rightEnc.reset();
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -134,14 +122,15 @@ public class Macadamia extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -158,23 +147,24 @@ public class Macadamia extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    if (timer.get() < 2.0) {
-        drive.curvatureDrive(0.1, 0.0, false);
-      }
-      else
-      {
-        drive.curvatureDrive(0.0, 0.0,false);
-      }
- 
- 
+    double distance = frontUltrasonic.getRangeInches();
+
+    if (distance < safeDistance) {
+      drive.tankDrive(-0.2, -0.2);
+    } else if (distance > safeDistance) {
+      drive.tankDrive(0.2, 0.2);
+    } else {
+      drive.tankDrive(0.0, 0.0);
+    }
+
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
@@ -182,78 +172,81 @@ public class Macadamia extends TimedRobot {
    * This function is called periodically during operator control.
    */
   @Override
-public void teleopPeriodic() {
-  // Use controller joysticks to set drive speed, but
-  // safety stop if too close to an obstacle
+  public void teleopPeriodic() {
+    // Use controller joysticks to set drive speed, but
+    // safety stop if too close to an obstacle
 
-  if (xbox.getYButtonPressed()) { // drive 2 feet forward
-    // reset encoders so counts start at 0
-    leftEnc.reset();
-    rightEnc.reset();
+    if (xbox.getStartButtonPressed()) { // go for an autonomous drive
+      DriveAround d = new DriveAround();
+      d.start();
+    } else if (xbox.getYButtonPressed()) { // drive 2 feet forward
+      // reset encoders so counts start at 0
+      leftEnc.reset();
+      rightEnc.reset();
 
-    // set motor speed to go straight forward
-    leftSpeed = 0.45;
-    rightSpeed = 0.5;
+      // set motor speed to go straight forward
+      leftSpeed = 0.45;
+      rightSpeed = 0.5;
 
-    // set point we want to stop at
-    stopLeft = 2000; stopRight = 2000;
+      // set point we want to stop at
+      stopLeft = 1000;
+      stopRight = 1000;
+    } else if (xbox.getBButtonPressed()) { // turn 90 degrees to right
+      // reset encoders so counts start at 0
+      leftEnc.reset();
+      rightEnc.reset();
+
+      // set motor speed to go straight forward
+      leftSpeed = 0.5;
+      rightSpeed = -0.47;
+
+      // set point we want to stop at
+      stopLeft = 666;
+      stopRight = -666;
+    } else if (xbox.getAButtonPressed()) { // drive backward 2 feet
+      // reset encoders so counts start at 0
+      leftEnc.reset();
+      rightEnc.reset();
+
+      // set motor speed to go straight forward
+      leftSpeed = -0.5;
+      rightSpeed = -0.47;
+
+      // set point we want to stop at
+      stopLeft = 1000;
+      stopRight = 1000;
+    } else if (xbox.getXButtonPressed()) { // turn 90 degrees to left
+      // reset encoders so counts start at 0
+      leftEnc.reset();
+      rightEnc.reset();
+
+      // set motor speed to go straight forward
+      rightSpeed = 0.5;
+      leftSpeed = -0.47;
+
+      // set point we want to stop at
+      stopLeft = -666;
+      stopRight = 666;
+    } else if (stopLeft == 0 && stopRight == 0) { // drive only using joystick
+      joystickDrive();
+    } else { // drive fixed distance
+      driveEncoder(stopLeft, stopRight, leftSpeed, rightSpeed);
+    }
   }
-  else if (xbox.getBButtonPressed()) {  // turn 90 degrees to right
-    // reset encoders so counts start at 0
-    leftEnc.reset();
-    rightEnc.reset();
 
-    // set motor speed to go straight forward
-    leftSpeed = 0.5;
-    rightSpeed = -0.47;
+  public void joystickDrive() {
+    leftSpeed = -0.5 * xbox.getY(Hand.kLeft);
+    rightSpeed = -0.5 * xbox.getY(Hand.kRight);
 
-    // set point we want to stop at
-    stopLeft = 2000; stopRight = 2000;
-  }
-  else if (xbox.getAButtonPressed()) { // drive backward 2 feet
-    // reset encoders so counts start at 0
-    leftEnc.reset();
-    rightEnc.reset();
-
-    // set motor speed to go straight forward
-    leftSpeed = - 0.5;
-    rightSpeed = -0.47;
-
-    // set point we want to stop at
-    stopLeft = 2000; stopRight = 2000;
-  } else if (xbox.getXButtonPressed()) {
-    // reset encoders so counts start at 0
-    leftEnc.reset();
-    rightEnc.reset();
-
-    // set motor speed to go straight forward
-    rightSpeed = 0.5;
-    leftSpeed = -0.47;
-
-    // set point we want to stop at
-    stopLeft = 2000; stopRight = 2000;
-  }
-  else if (stopLeft == 0 && stopRight == 0) { // drive only using joystick
-    joystickDrive();
-  } else {  // drive fixed distance
-    driveEncoder(stopLeft, stopRight, leftSpeed, rightSpeed);
-  }
-}
-
-public void joystickDrive() {
-  leftSpeed  = -0.5*xbox.getY(Hand.kLeft);
-  rightSpeed = -0.5*xbox.getY(Hand.kRight);
-
-  if (safetyStop(safeDistance, frontUltrasonic) && (leftSpeed > 0.0) && (rightSpeed > 0.0)) {
+    if (safetyStop(safeDistance, frontUltrasonic) && (leftSpeed > 0.0) && (rightSpeed > 0.0)) {
       drive.stopMotor();
-  } else if (safetyStop(safeDistance, rearUltrasonic) && (leftSpeed < 0.0) && (rightSpeed < 0.0)) {
+    } else if (safetyStop(safeDistance, rearUltrasonic) && (leftSpeed < 0.0) && (rightSpeed < 0.0)) {
       drive.stopMotor();
-  } else {
-    // otherwise, set motors according to joysticks
+    } else {
+      // otherwise, set motors according to joysticks
       drive.tankDrive(leftSpeed, rightSpeed);
+    }
   }
-}
-
 
   /**
    * This function is called periodically during test mode.
@@ -262,54 +255,72 @@ public void joystickDrive() {
   public void testPeriodic() {
   }
 
-
-  // Use  ultrasonic sensor to stop robot
-  // if it gets too close to an obstacle 
+  // Use ultrasonic sensor to stop robot
+  // if it gets too close to an obstacle
   public boolean safetyStop(double safeDistance, Ultrasonic2537 sensor) {
 
-    if (sensor == null)  // no ultrasonic sensor working
+    if (sensor == null) // no ultrasonic sensor working
       return false;
 
     double distance = sensor.getRangeInches();
-    if (distance < safeDistance) 
-        return true;
+    if (distance < safeDistance)
+      return true;
     else
-        return false;
-}
-
-
-public void driveS(){
- driveEncoder(2000,2000, 0.47,0.5);
-}
-public void driveRight(){
-  driveEncoder(666,-666, 0.5,-0.47);
-}
-public void driveBack(){
-  driveEncoder(-2000,-2000, -0.47,-0.5);
-}
-public void driveLeft(){
-  driveEncoder(-666,666, -0.47,0.5);
-}
-
-void driveEncoder(int leftDistance, int rightDistance, double leftSpeed, double rightSpeed){
-  leftEnc.reset();
-  rightEnc.reset();
-  if ((Math.abs(leftEnc.getRaw()) < Math.abs(leftDistance)) ||
-        (Math.abs(rightEnc.getRaw()) < Math.abs(rightDistance))){
-          //System.out.println("left" + leftEnc.getRaw() + "right" + rightEnc.getRaw());
-          if (Math.abs(leftEnc.getRaw()) >= Math.abs(leftDistance)){
-            drive.tankDrive(0.0,rightSpeed * 0.2);
-          }
-          else if (Math.abs(rightEnc.getRaw()) >= Math.abs(rightDistance)){
-            drive.tankDrive(leftSpeed * 0.2,0.0);
-          }
-          else {
-            drive.tankDrive(leftSpeed,rightSpeed);
-          } 
-        }
-  else {
-    drive.tankDrive(0.0,0.0);
+      return false;
   }
-}
+
+     /**
+   * Drive forward the specified number of inches. Assume circumfrence of drive wheels is 23.75 inches
+   */ 
+  public void driveForwardInches(int distance) {
+    int distanceTicks = (int) (distance * 1000 / 23.75);
+
+    driveEncoder(distanceTicks, distanceTicks, 0.47, 0.5);
+  }
+
+  /**
+   * Turn the robot 90 degrees to the left in place.
+   */ 
+  public void turnRight() {
+    driveEncoder(666, -666, 0.5, -0.47);
+  }
+
+  /**
+   * Drive backward the specified number of inches. Assume circumfrence of drive wheels is 23.75 inches
+   */ 
+  public void driveBackwardInches(int distance) {
+    int distanceTicks = (int) (distance * -1000 / 23.75);
+
+    driveEncoder(distanceTicks, distanceTicks, -0.47, -0.5);
+  }
+
+   /**
+   * Turn the robot 90 degrees to the right in place.
+   */ 
+  public void turnLeft() {
+    driveEncoder(-666, 666, -0.47, 0.5);
+  }
+
+  /**
+   * drive a specified distance at a specified speed. Use negative distance/speed to go backward.
+   * Can turn by using negative and positive speed values.
+   */
+  void driveEncoder(int leftDistance, int rightDistance, double leftSpeed, double rightSpeed) {
+    leftEnc.reset();
+    rightEnc.reset();
+    if ((Math.abs(leftEnc.getRaw()) < Math.abs(leftDistance))
+        || (Math.abs(rightEnc.getRaw()) < Math.abs(rightDistance))) {
+      // System.out.println("left" + leftEnc.getRaw() + "right" + rightEnc.getRaw());
+      if (Math.abs(leftEnc.getRaw()) >= Math.abs(leftDistance)) {
+        drive.tankDrive(0.0, rightSpeed * 0.2);
+      } else if (Math.abs(rightEnc.getRaw()) >= Math.abs(rightDistance)) {
+        drive.tankDrive(leftSpeed * 0.2, 0.0);
+      } else {
+        drive.tankDrive(leftSpeed, rightSpeed);
+      }
+    } else {
+      drive.tankDrive(0.0, 0.0);
+    }
+  }
 
 };
